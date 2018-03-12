47b63406292897d5eabe0337597617dc5607405c
zory95 <rubencalatorao@gmail.com>
2018-03-12 11:30:45 +0100

    Spanish added
    Modified to work with the library "pattern". Replace Spanish module to modify language

--- quepy/nltktagger.py
+++ quepy/nltktagger.py
@@ -8,74 +8,23 @@
 #          Gonzalo Garcia Berrotaran <ggarcia@machinalis.com>
 
 """
-Tagging using NLTK.
+Modified to be able to use it in Spanish
+Needed library: pattern
 """
 
-# Requiered data files are:
-#   - "averaged_perceptron_tagger" in Models
-#   - "wordnet" in Corpora
-
-import nltk
 from quepy.tagger import Word
-from quepy.encodingpolicy import assert_valid_encoding
 
-_penn_to_morphy_tag = {}
-
-
-def penn_to_morphy_tag(tag):
-    assert_valid_encoding(tag)
-
-    for penn, morphy in _penn_to_morphy_tag.iteritems():
-        if tag.startswith(penn):
-            return morphy
-    return None
-
+# Replace Spanish module to modify language
+from pattern.es import parsetree
 
 def run_nltktagger(string, nltk_data_path=None):
-    """
-    Runs nltk tagger on `string` and returns a list of
-    :class:`quepy.tagger.Word` objects.
-    """
-    assert_valid_encoding(string)
-    global _penn_to_morphy_tag
-
-    if nltk_data_path:
-        nltk.data.path = nltk_data_path
-
-    from nltk.corpus import wordnet
-
-    if not _penn_to_morphy_tag:
-        _penn_to_morphy_tag = {
-            u'NN': wordnet.NOUN,
-            u'JJ': wordnet.ADJ,
-            u'VB': wordnet.VERB,
-            u'RB': wordnet.ADV,
-        }
-
-    # Recommended tokenizer doesn't handle non-ascii characters very well
-    #tokens = nltk.word_tokenize(string)
-    tokens = nltk.wordpunct_tokenize(string)
-    tags = nltk.pos_tag(tokens)
-
+    parsed = parsetree(string, lemmata=True)
     words = []
-    for token, pos in tags:
-        word = Word(token)
-        # Eliminates stuff like JJ|CC
-        # decode ascii because they are the penn-like POS tags (are ascii).
-        word.pos = pos.split("|")[0].decode("ascii")
-
-        mtag = penn_to_morphy_tag(word.pos)
-        # Nice shooting, son. What's your name?
-        lemma = wordnet.morphy(word.token, pos=mtag)
-        if isinstance(lemma, str):
-            # In this case lemma is example-based, because if it's rule based
-            # the result should be unicode (input was unicode).
-            # Since english is ascii the decoding is ok.
-            lemma = lemma.decode("ascii")
-        word.lemma = lemma
-        if word.lemma is None:
-            word.lemma = word.token.lower()
-
-        words.append(word)
+    for sentence in parsed:
+        for w in list(sentence):
+            word = Word(w.string)
+            word.pos = w.type
+            word.lemma = w.lemma
+            words.append(word)
 
     return words
